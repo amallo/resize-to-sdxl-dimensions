b01cfec9d317225fc809f03e4dff979288b329bf
amallo <audie@app2b.io>
2025-02-25 18:24:29 +0100

    resize image to nearest allowed dimension

---
+++ README.md
@@ -0,0 +1,21 @@
+
+The "Resize Image SDXL" node is an extension to the conventional "Image Resize" node, that resize image to
+the nearest allowed dimensions compatible with Stable Diffusion XL.
+
+* 1024 x 1024
+
+* 1152 x 896
+
+* 896 x 1152
+
+* 1216 x 832
+
+* 832 x 1216
+
+* 1344 x 768
+
+* 768 x 1344
+
+* 1536 x 640
+
+* 640 x 1536
---
+++ resize-sdxl-allowed-dimensions.py
@@ -0,0 +1,49 @@
+from invokeai.invocation_api import (
+    BaseInvocation,
+    InputField,
+    InvocationContext,
+    ImageField,
+    ImageOutput
+)
+
+
+class ResizeImagePlusInvocation(BaseInvocation):
+    """Resizes an image to specific dimensions"""
+    image: ImageField = InputField(default=None, description="Image to be resize")
+   
+
+    def resize_to_allowed_dimensions(width, height):
+        """
+        Function re-used from Lucataco's implementation of SDXL-Controlnet for Replicate
+        """
+        # List of SDXL dimensions
+        allowed_dimensions = [
+            (512, 2048), (512, 1984), (512, 1920), (512, 1856),
+            (576, 1792), (576, 1728), (576, 1664), (640, 1600),
+            (640, 1536), (704, 1472), (704, 1408), (704, 1344),
+            (768, 1344), (768, 1280), (832, 1216), (832, 1152),
+            (896, 1152), (896, 1088), (960, 1088), (960, 1024),
+            (1024, 1024), (1024, 960), (1088, 960), (1088, 896),
+            (1152, 896), (1152, 832), (1216, 832), (1280, 768),
+            (1344, 768), (1408, 704), (1472, 704), (1536, 640),
+            (1600, 640), (1664, 576), (1728, 576), (1792, 576),
+            (1856, 512), (1920, 512), (1984, 512), (2048, 512)
+        ]
+        # Calculate the aspect ratio
+        aspect_ratio = width / height
+        
+        # Find the closest allowed dimensions that maintain the aspect ratio
+        closest_dimensions = min(
+            allowed_dimensions,
+            key=lambda dim: abs(dim[0] / dim[1] - aspect_ratio)
+        )
+        return closest_dimensions
+
+    def invoke(self, context: InvocationContext) -> ImageOutput:
+        image = context.images.get_pil(self.image.image_name)
+        image_width, image_height = image.size
+        new_width, new_height = self.resize_to_allowed_dimensions(image_width, image_height)
+        image_out = image.resize((new_width, new_height))
+        image_dto = context.images.save(image=image_out)
+        return ImageOutput.build(image_dto)
+        
